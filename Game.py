e78fdaee7531928ee9d50b307244ef00ce09e7ae
Anonymoustank <pkadekodi@gmail.com>
2020-07-01 10:18:35 -0600

    Platform can't travel off screen anymore

--- Game.py
+++ Game.py
@@ -36,41 +36,43 @@ def refresh(time):
     space.step(time)
     if left_pressed == True and right_pressed == False:
         x, y = player.position
-        player.position = x - 5, y
-        x,y = body.position
-        body.position = x - 5, y
+        if x > 125:
+            player.position = x - 5, y
+            x,y = body.position
+            body.position = x - 5, y
     elif right_pressed == True and left_pressed == False:
         x, y = player.position
-        player.position = x + 5, y
-        x,y = body.position
-        body.position = x + 5, y
+        if x < 1155:
+            player.position = x + 5, y
+            x,y = body.position
+            body.position = x + 5, y
 
 @window.event
 def on_key_press(symbol, modifiers):
     global left_pressed, right_pressed
     if symbol == key.A or symbol == key.LEFT:
-        x, y = player.position
-        player.position = x - 5, y
-        x,y = body.position
-        body.position = x - 5, y
         left_pressed = True
         right_pressed = False
-    elif symbol == key.D or symbol == key.RIGHT:
         x, y = player.position
-        player.position = x + 5, y
-        x,y = body.position
-        body.position = x + 5, y
+        if x > 125:
+            player.position = x - 5, y
+            x,y = body.position
+            body.position = x - 5, y
+    elif symbol == key.D or symbol == key.RIGHT:
         left_pressed = False
         right_pressed = True
+        x, y = player.position
+        if x < 1155:
+            player.position = x + 5, y
+            x,y = body.position
+            body.position = x + 5, y
 
 @window.event
 def on_key_release(symbol, modifiers):
     global left_pressed, right_pressed
     if symbol == key.A or symbol == key.LEFT:
         left_pressed = False
-        right_pressed = False
     if symbol == key.D or symbol == key.RIGHT:
-        left_pressed = False
         right_pressed = False
     
 if __name__ == "__main__":
